47a57f3a1eb96c0ce16a6965e6906aeb61685086
Maks <maksterekhov1704@gmail.com>
2021-02-06 20:40:23 +0200

    вынес точки появления в LevelCore

--- Assets/Flatout/Scripts/Gameplay/LevelCore/LevelCore.cs
+++ Assets/Flatout/Scripts/Gameplay/LevelCore/LevelCore.cs
@@ -130,7 +130,7 @@ namespace Flatout
         {
             foreach (var spawnpoint in spawnPoints)
             {
-                var botLevelBase = PlayerAvatar.Instance.hardnessLevel.GetBotCar(PlayerAvatar.Instance.Level);
+                var botLevelBase = PlayerCarTier;//PlayerAvatar.Instance.hardnessLevel.GetBotCar(PlayerAvatar.Instance.Level);
                 var botGameObject = Instantiate(botLevelBase.CarPrefab, spawnpoint);
                 var botBaseComponennt = botGameObject.AddComponent<BotCar>();
                 var botAIComponent = botGameObject.AddComponent<CarAIControl>();
